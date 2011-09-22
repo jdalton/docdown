45c1e884d82461a6a8e37ad50a9addc3109cbea3
John-David Dalton <john.david.dalton@gmail.com>
2011-09-22 06:13:52 -0400

    docdown: Code cleanup. [jddalton]

--- docs/parse.php
+++ docs/parse.php
@@ -7,8 +7,11 @@
 
   // output filename
   $output = isset($_GET['o'])
-    ? $_GET['o'] : isset($_SERVER['argv'][1])
-    ? $_SERVER['argv'][1] : basename($file);
+    ? $_GET['o']
+    : isset($_SERVER['argv'][1])
+      ? $_SERVER['argv'][1]
+      : basename($file);
+
   /*--------------------------------------------------------------------------*/
 
   require('../docdown.php');
