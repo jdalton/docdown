7928a8139d1d13b1317775c4147d5ebb434e7593
John-David Dalton <john.david.dalton@gmail.com>
2014-12-07 16:42:44 -0800

    Remove IIFEs.

--- doc/parse.js
+++ doc/parse.js
@@ -1,29 +1,27 @@
-(function() {
+'use strict';
 
-  var fs = require("fs"),
-      path = require("path");
+var fs = require("fs"),
+    path = require("path");
 
-  var docdown = require("../index.js");
+var docdown = require("../index.js");
 
-  // input filename
-  var file = process.argv[2];
-  file += /\.[a-z]+$/.test(file) ? '' : '.js';
+// input filename
+var file = process.argv[2];
+file += /\.[a-z]+$/.test(file) ? '' : '.js';
 
-  // output filename
-  var output = process.argv[3] || path.basename(file);
+// output filename
+var output = process.argv[3] || path.basename(file);
 
-  /*--------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
-  // generate Markdown
-  var markdown = docdown({
-    'path': path.join(process.cwd(), file),
-    'title': 'Docdown <sup>v1.0.0</sup>',
-    'url': 'https://github.com/jdalton/docdown/blob/master/index.js'
-  });
+// generate Markdown
+var markdown = docdown({
+  'path': path.join(process.cwd(), file),
+  'title': 'Docdown <sup>v1.0.0</sup>',
+  'url': 'https://github.com/jdalton/docdown/blob/master/index.js'
+});
 
-  // save to a .md file
-  fs.writeFileSync(output + '.md', markdown);
+// save to a .md file
+fs.writeFileSync(output + '.md', markdown);
 
-  console.log(markdown + '\n');
-
-}());
+console.log(markdown + '\n');
--- index.js
+++ index.js
@@ -3,38 +3,34 @@
  * Copyright 2011-2014 John-David Dalton <http://allyoucanleet.com/>
  * Available under MIT license <http://mths.be/mit>
  */
+'use strict';
 
-;(function() {
-  'use strict';
+var _ = require('lodash'),
+    path = require('path'),
+    fs = require('fs'),
+    generateDoc = require('./lib/generator.js');
 
-  var _ = require('lodash'),
-      path = require('path'),
-      fs = require('fs'),
-      generateDoc = require('./lib/generator.js');
-
-  /**
-   * Generates Markdown documentation based on JSDoc comments.
-   *
-   * @name docdown
-   * @param options The options to use to generate documentation.
-   * @returns {string} The generated Markdown code.
-   */
-  function docdown(options) {
-    if (!options.path || !options.url) {
-      throw new Error("Path and/or URL must be specified");
-    }
-
-    _.defaults(options, {
-      toc   : 'properties',
-      lang  : 'js',
-      title : path.basename(options.path) + ' API documentation'
-    });
-
-    return generateDoc(
-      fs.readFileSync(options.path, 'utf8'),
-      options
-    );
+/**
+ * Generates Markdown documentation based on JSDoc comments.
+ *
+ * @name docdown
+ * @param options The options to use to generate documentation.
+ * @returns {string} The generated Markdown code.
+ */
+function docdown(options) {
+  if (!options.path || !options.url) {
+    throw new Error("Path and/or URL must be specified");
   }
+  _.defaults(options, {
+    'toc': 'properties',
+    'lang': 'js',
+    'title': path.basename(options.path) + ' API documentation'
+  });
 
-  module.exports = docdown;
-}());
+  return generateDoc(
+    fs.readFileSync(options.path, 'utf8'),
+    options
+  );
+}
+
+module.exports = docdown;
