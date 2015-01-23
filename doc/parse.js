d52f3840d3dc5aca6717953a3047d5ee186a90ca
jdalton <john.david.dalton@gmail.com>
2015-01-22 21:36:50 -0800

    Update comment style.

--- doc/parse.js
+++ doc/parse.js
@@ -5,23 +5,21 @@ var fs = require("fs"),
 
 var docdown = require("../index.js");
 
-// input filename
+// The input filename.
 var file = process.argv[2];
 file += /\.[a-z]+$/.test(file) ? '' : '.js';
 
-// output filename
+// The output filename.
 var output = process.argv[3] || path.basename(file);
 
 /*----------------------------------------------------------------------------*/
 
-// generate Markdown
 var markdown = docdown({
   'path': path.join(process.cwd(), file),
   'title': 'Docdown <sup>v1.0.0</sup>',
   'url': 'https://github.com/jdalton/docdown/blob/master/index.js'
 });
 
-// save to a .md file
 fs.writeFileSync(output + '.md', markdown);
 
 console.log(markdown + '\n');
