82102a5d057632900270d1afb4e3127151597838
jdalton <john.david.dalton@gmail.com>
2015-02-02 22:30:05 -0800

    Make parse.js read package.json for the version number.

--- doc/parse.js
+++ doc/parse.js
@@ -1,25 +1,32 @@
 'use strict';
 
-var fs = require("fs"),
-    path = require("path");
+var fs = require('fs'),
+    path = require('path'),
+    docdown = require('../index.js');
 
-var docdown = require("../index.js");
+var packagePath = path.resolve(__dirname, '..', 'package.json'),
+    packageText = fs.readFileSync(packagePath, 'utf-8'),
+    packageJSON = JSON.parse(packageText);
+
+var reExt = /\.[a-z]+$/;
+
+// The version number from the package.json.
+var version = packageJSON.version;
 
 // The input filename.
-var file = process.argv[2];
-file += /\.[a-z]+$/.test(file) ? '' : '.js';
+var file = process.argv[2] + (reExt.test(process.argv[2]) ? '' : '.js');
 
 // The output filename.
-var output = process.argv[3] || path.basename(file);
+var output = process.argv[3] || (path.basename(file).replace(reExt, '') + '.md')
 
 /*----------------------------------------------------------------------------*/
 
 var markdown = docdown({
   'path': path.join(process.cwd(), file),
-  'title': 'Docdown <sup>v1.0.0</sup>',
-  'url': 'https://github.com/jdalton/docdown/blob/master/index.js'
+  'title': 'Docdown <sup>v' + version + '</sup>',
+  'url': 'https://github.com/jdalton/docdown/tree/' + version + '/index.js'
 });
 
-fs.writeFileSync(output + '.md', markdown);
+fs.writeFileSync(output, markdown);
 
 console.log(markdown + '\n');
